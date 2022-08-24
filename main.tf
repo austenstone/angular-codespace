225c07985c1be35eca8c3619fa7d4c21c8b728e0
Austen Stone <austenstone@github.com>
2022-08-23 22:22:04 -0400

    test

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ resource "azurerm_resource_group" "austenstone-rg" {
 
 resource "azurerm_static_site" "web" {
   name                = "angular-web"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.austenstone-rg.location
+  resource_group_name = azurerm_resource_group.austenstone-rg.name
 }
